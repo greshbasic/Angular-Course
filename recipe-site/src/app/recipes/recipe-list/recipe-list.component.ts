c997385d72c4c826a194380b1f5ccfd248ecf2a8
Gresh Basic <basic.gresham@gmail.com>
2024-05-22 11:50:33 -0500

    mo dogs

--- recipe-site/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-site/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
 
+    recipes: Recipe[] = [
+      new Recipe('Hotdog', 'A good ole hotdog', "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"),
+      new Recipe('Hotdog', 'A good ole hotdog', "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"),
+      new Recipe('Hotdog', 'A good ole hotdog', "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"),
+
+    ];
+
+
+    constructor() {
+
+    }
+    ngOnInit(): void {
+      
+    }
 }
