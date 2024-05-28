a02bc61047a492800edba2d73673b6d85d06bee4
Gresh Basic <basic.gresham@gmail.com>
2024-05-28 15:18:14 -0500

    hmm

---
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrl: './server-element.component.css'
+})
+export class ServerElementComponent {
+  @Input() element: {name: string,
+            type: string,
+            content: string}
+}
