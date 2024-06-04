b0774bd57f2efc7da270c1512da5dcd0472f921e
Gresh Basic <basic.gresham@gmail.com>
2024-06-04 13:40:11 -0500

    redo

--- recipe-site/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-site/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,25 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
 
-    recipes: Recipe[] = [
-      new Recipe('Hotdog', 'A good ole hotdog', "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"),
-      new Recipe('Hotdog', 'A good ole hotdog', "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"),
-      new Recipe('Hotdog', 'A good ole hotdog', "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"),
+  constructor() { }
 
-    ];
+  ngOnInit() {
+  }
 
+  onRecipeSelected(recipe: Recipe) {
+   this.recipeWasSelected.emit(recipe);
+  }
 
-    constructor() {
-
-    }
-    ngOnInit(): void {
-      
-    }
 }
