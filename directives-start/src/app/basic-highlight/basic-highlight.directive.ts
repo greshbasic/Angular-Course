692347e9a0bba4bc5399dd4de0407adb1c0f4935
Gresh Basic <basic.gresham@gmail.com>
2024-06-04 15:06:21 -0500

    dir

---
+++ directives-start/src/app/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,10 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appBasicHighlight]',
+})
+export class BasicHighlightDirective {
+  constructor(private elementRef: ElementRef) {
+    this.elementRef.nativeElement.style.backgroundColor = 'green';
+  }
+}
